7f0459c5f8657ad151e7707d96fd74c20a38464b
Stacia <staciaray@roadrunner.com>
2016-01-19 17:21:08 -0500

    added controllers

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -9,13 +9,19 @@
 		$stateProvider
 			.state('landing', {
 				url: '/',
+				controller: 'LandingCtrl as landing',
 				templateUrl: '/templates/landing.html' //the ui-view directive in index.html loads the landing state
 		})
 			.state('album', {
-			url: '/album',
-			templateUrl: '/templates/album.html' // when localhost:3000/album ui-view loads the album template
+				url: '/album',
+				templateUrl: '/templates/album.html' // when localhost:3000/album ui-view loads the album template
+		})
+			.state('collection', {
+				url: '/collection',
+				controller: 'CollectionCtrl as collection',
+				templateUrl: '/templates/collection.html'
 		});
-	}
+		
 		angular
 			module('blocJams', ['ui.router'])
 			.config(config);
---
+++ dist/scripts/controllers/LandingCtrl.js
@@ -0,0 +1,8 @@
+(function() {
+	function LandingCtrl() {
+		this.heroTitle = "Turn the Music Up!"; //'this' adds heroTitle property to LandingCtrl's $scope object
+	}
+	angular
+		.module('blocJams')
+	.controller('LandingCtrl', LandingCtrl);
+})();
