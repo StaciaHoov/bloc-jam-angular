f156146b5769b31b5f72fb2cec5257c79d6743cc
Stacia <staciaray@roadrunner.com>
2016-02-07 12:46:33 -0500

    next song function

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -29,8 +29,8 @@
 */
 		var setSong = function(song) {
 			if (currentBuzzObject) {
-					currentBuzzObject.stop();
-					SongPlayer.currentSong.playing = null;
+				currentBuzzObject.stop();
+				SongPlayer.currentSong.playing = null;
 			} 
 			currentBuzzObject = new buzz.sound(song.audioUrl, {
 				formats: ['mp3'],
@@ -43,10 +43,20 @@
 /**
 * @function playSong
 * @desc Plays the current audio file and sets the song object to playing
+* @param {Object} song
 */		
 		var playSong = function(song) {
 			currentBuzzObject.play();
 			song.playing = true;	
+		};
+/**
+* @function stopSong
+* @desc Stops the current audio file and sets the song object to null
+* @param {Object} song
+*/			
+		var stopSong = function(song) {
+			currentBuzzObject.stop();
+			song.playing = null;
 		};
 		
 	
@@ -79,7 +89,7 @@
 			song.playing = false;
 		};
 		
-		/**
+/**
 * @function SongPlayer.previous
 * @desc Gets the index of the song before the currently playing song. If song is first in album (index=0), stop audio file and set currentSong to not playing (null). If index >0, set and play the song associated with currentSongIndex.
 */
@@ -96,6 +106,24 @@
 				setSong(song);
 				playSong(song);
 			}
+		};
+
+/**		
+* @function SongPlayer.next
+* @desc Gets the index of and plays the song coming after the currently playing song. If song is last in album (index=length-1), stop audio file and set currentSong to not playing (null). 
+*/		
+		SongPlayer.next = function() {
+			var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+			currentSongIndex++;
+			
+			if (currentSongIndex > (songs.length - 1)) {
+				currentBuzzObject.stop();
+				SongPlayer.currentSong.playing = null;
+			} else {
+				var song = currentAlbum.songs[currentSongIndex];
+				setSong(song);
+				playSong(song);
+			}			
 		};
 		
 		return SongPlayer;
