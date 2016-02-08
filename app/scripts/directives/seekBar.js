bf1b07be2d1a2d154dab7c76dbe9d60e34e29ed7
Stacia <staciaray@roadrunner.com>
2016-02-08 18:21:25 -0500

    assignment completed

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -1,13 +1,9 @@
 (function() {
-<<<<<<< HEAD
-	function seekBar() {
-=======
-	
+	function seekBar($document) {
 /*
 * @desc Calculates the horizontal percent along the seek bar where 
 * the event (passed in from the view as $event) occurred.
 */
-	function seekBar($document) {
 		var calculatePercent = function(seekBar, event) {
 			var offsetX = event.pageX - seekBar.offset().left;
 			var seekBarWidth = seekBar.width();
@@ -16,7 +12,7 @@
 			offsetXPercent = Math.min(1, offsetXPercent);
 			return offsetXPercent;
 		};
-		
+
 		return {
 			templateUrl: '/templates/directives/seek_bar.html',
 			replace: true,
@@ -25,24 +21,28 @@
 			link: function(scope, element, attributes) {
 				scope.value = 0;
 				scope.max = 100;
-				
+
 /*
 * @desc Holds the element that matches the directive (<seek-bar>) as a 
 * jQuery object so we can call jQuery methods on it.
 */
 				var seekBar = $(element);
-				
+
 				var percentString = function() {
 					var value = scope.value;
 					var max = scope.max;
 					var percent = value / max * 100;
 					return percent + "%";
 				};
-				
+
 				scope.fillStyle = function() {
 					return {width: percentString()};
 				};
-				
+
+				scope.thumbStyle = function() {
+					return {left: percentString()}
+				};
+
 /*
 * @desc Updates the seek bar value based on the seek bar's width and the 
 * location of the user's click on the seek bar.
@@ -51,7 +51,7 @@
 					var percent = calculatePercent(seekBar, event);
 					scope.value = percent * scope.max;
 				};
-	
+
 /* 
 * uses $apply to constantly apply the change in value of scope.value as the user drags the seek bar thumb.
 */
@@ -66,7 +66,7 @@
 						$document.unbind('mousemove.thumb');
 						$document.unbind('mouseup.thumb');
 					});
-					
+
 				};
 			}
 		};
@@ -75,5 +75,4 @@
 	angular
 		.module('blocJams')
 		.directive('seekBar', ['$document', seekBar]);
-
 })();
--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -1,13 +1,9 @@
 (function() {
-<<<<<<< HEAD
-	function seekBar() {
-=======
-	
+	function seekBar($document) {
 /*
 * @desc Calculates the horizontal percent along the seek bar where 
 * the event (passed in from the view as $event) occurred.
 */
-	function seekBar($document) {
 		var calculatePercent = function(seekBar, event) {
 			var offsetX = event.pageX - seekBar.offset().left;
 			var seekBarWidth = seekBar.width();
@@ -16,39 +12,37 @@
 			offsetXPercent = Math.min(1, offsetXPercent);
 			return offsetXPercent;
 		};
-		
->>>>>>> checkpoint-9-directives-part1
+
 		return {
 			templateUrl: '/templates/directives/seek_bar.html',
 			replace: true,
 			restrict: 'E',
-<<<<<<< HEAD
-			scope: { },
-			link: function(scope, element, attributes) {
-				
-=======
 			scope: {},
 			link: function(scope, element, attributes) {
 				scope.value = 0;
 				scope.max = 100;
-				
+
 /*
 * @desc Holds the element that matches the directive (<seek-bar>) as a 
 * jQuery object so we can call jQuery methods on it.
 */
 				var seekBar = $(element);
-				
+
 				var percentString = function() {
 					var value = scope.value;
 					var max = scope.max;
 					var percent = value / max * 100;
 					return percent + "%";
 				};
-				
+
 				scope.fillStyle = function() {
 					return {width: percentString()};
 				};
-				
+
+				scope.thumbStyle = function() {
+					return {left: percentString()}
+				};
+
 /*
 * @desc Updates the seek bar value based on the seek bar's width and the 
 * location of the user's click on the seek bar.
@@ -57,7 +51,7 @@
 					var percent = calculatePercent(seekBar, event);
 					scope.value = percent * scope.max;
 				};
-	
+
 /* 
 * uses $apply to constantly apply the change in value of scope.value as the user drags the seek bar thumb.
 */
@@ -72,19 +66,13 @@
 						$document.unbind('mousemove.thumb');
 						$document.unbind('mouseup.thumb');
 					});
-					
+
 				};
->>>>>>> checkpoint-9-directives-part1
 			}
 		};
 	}
 	
 	angular
 		.module('blocJams')
-<<<<<<< HEAD
-		.directive('seekBar', seekBar);
-=======
 		.directive('seekBar', ['$document', seekBar]);
-	
->>>>>>> checkpoint-9-directives-part1
 })();
